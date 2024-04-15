84cacc2a3a43a7645405b5f20ac8a4791f0a0a45
Astraeus <astraeuszhao@gmail.com>
2024-04-15 15:04:24 +0800

    chore: lint whitelist whitelist sol

--- src/Whitelist/Whitelist.sol
+++ src/Whitelist/Whitelist.sol
@@ -9,3 +9,24 @@ contract Whitelist {
     error NotOwner(address caller);
     error ZeroAddress();
     error AlreadyWhitelisted(address account);
+    error NotWhitelisted(address account);
+
+    event Whitelisted(address indexed account);
+    event WhitelistRemoved(address indexed account);
+
+    address public owner;
+    mapping(address account => bool listed) public whitelisted;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner(msg.sender);
+        _;
+    }
+
+    modifier onlyWhitelisted() {
+        if (!whitelisted[msg.sender]) revert NotWhitelisted(msg.sender);
+        _;
+    }
