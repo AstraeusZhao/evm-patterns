1ce90e115bcece92fbf708d8b4ef845ad041b647
Astraeus <astraeuszhao@gmail.com>
2024-05-05 12:41:13 +0800

    test: stabilize ownable ownable t sol

--- test/Ownable/Ownable.t.sol
+++ test/Ownable/Ownable.t.sol
@@ -5,3 +5,18 @@ import {Ownable} from "../../src/Ownable/Ownable.sol";
 
 interface Vm {
     function prank(address sender) external;
+    function expectRevert() external;
+}
+
+contract OwnableTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant OWNER = address(0xB055);
+    address private constant OTHER = address(0x07E);
+
+    function _ownable() internal returns (Ownable o) {
+        vm.prank(OWNER);
+        o = new Ownable();
+    }
+
+    function testDeployerIsOwner() external {
