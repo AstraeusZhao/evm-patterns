296683be827b5005687f87a03f6da7cc979f5aeb
Astraeus <astraeuszhao@gmail.com>
2024-05-09 10:12:45 +0800

    test: fix pausable pausable t sol expectations

--- test/Pausable/Pausable.t.sol
+++ test/Pausable/Pausable.t.sol
@@ -5,3 +5,10 @@ import {Pausable} from "../../src/Pausable/Pausable.sol";
 
 interface Vm {
     function prank(address sender) external;
+    function expectRevert() external;
+}
+
+contract PausableTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant OWNER = address(0xB055);
