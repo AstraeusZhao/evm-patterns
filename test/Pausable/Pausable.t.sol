edd58b58608ab526f9640df37a7451732e29a7fa
Astraeus <astraeuszhao@gmail.com>
2024-05-10 10:14:46 +0800

    test: stabilize pausable pausable t sol

--- test/Pausable/Pausable.t.sol
+++ test/Pausable/Pausable.t.sol
@@ -12,3 +12,16 @@ contract PausableTest {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
     address private constant OWNER = address(0xB055);
+    address private constant OTHER = address(0x07E);
+
+    function _pausable() internal returns (Pausable p) {
+        vm.prank(OWNER);
+        p = new Pausable();
+    }
+
+    function testPauseAndUnpause() external {
+        Pausable p = _pausable();
+        vm.prank(OWNER);
+        p.pause();
+        require(p.paused(), "not paused");
+        vm.prank(OWNER);
