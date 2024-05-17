670c64ed21e7c2a1284ce64a913c0f4bfbc8c4ae
Astraeus <astraeuszhao@gmail.com>
2024-05-17 17:49:54 +0800

    test: fix whitelist whitelist t sol expectations

--- test/Whitelist/Whitelist.t.sol
+++ test/Whitelist/Whitelist.t.sol
@@ -11,3 +11,28 @@ interface Vm {
 
 contract WhitelistTest {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant OWNER = address(0xB055);
+    address private constant ALICE = address(0xA11CE);
+    address private constant BOB = address(0xB0B);
+
+    function _whitelist() internal returns (Whitelist) {
+        vm.deal(ALICE, 5 ether);
+        vm.deal(BOB, 5 ether);
+        vm.prank(OWNER);
+        return new Whitelist();
+    }
+
+    function testAddAndRemove() external {
+        Whitelist w = _whitelist();
+        vm.prank(OWNER);
+        w.addToWhitelist(ALICE);
+        require(w.whitelisted(ALICE), "not listed");
+
+        vm.prank(OWNER);
+        w.removeFromWhitelist(ALICE);
+        require(!w.whitelisted(ALICE), "still listed");
+    }
+
+    function testOnlyOwnerManages() external {
+        Whitelist w = _whitelist();
