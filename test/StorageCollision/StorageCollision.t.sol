0668e28a0f9aa8874f519e33815008968beb3dc2
Astraeus <astraeuszhao@gmail.com>
2024-06-21 09:00:13 +0800

    test: add storagecollision storagecollision t sol

---
+++ test/StorageCollision/StorageCollision.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.24;
+
+import {StorageCollisionProxy, ImplValueSlot0, ImplValueSlot1} from "../../src/StorageCollision/StorageCollision.sol";
+
+interface Vm {
