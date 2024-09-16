0fd31b0aafbcd47e0b4b6629f869ea9b6b21bed5
Astraeus <astraeuszhao@gmail.com>
2024-09-16 11:36:47 +0800

    fix: tighten storagecollision storagecollision sol

--- src/StorageCollision/StorageCollision.sol
+++ src/StorageCollision/StorageCollision.sol
@@ -21,3 +21,25 @@ contract StorageCollisionProxy {
     modifier onlyOwner() {
         if (msg.sender != owner) revert NotOwner(msg.sender);
         _;
+    }
+
+    function setImplementation(address impl) external onlyOwner {
+        implementation = impl;
+    }
+
+    fallback() external payable {
+        _delegate(implementation);
+    }
+
+    receive() external payable {
+        _delegate(implementation);
+    }
+
+    function _delegate(address impl) internal {
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
