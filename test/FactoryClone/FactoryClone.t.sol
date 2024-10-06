51e620a69cda6aa3c2eed72f1fc406c177c728a2
Astraeus <astraeuszhao@gmail.com>
2024-10-06 12:42:55 +0800

    test: fix factoryclone factoryclone t sol expectations

--- test/FactoryClone/FactoryClone.t.sol
+++ test/FactoryClone/FactoryClone.t.sol
@@ -5,3 +5,17 @@ import {FactoryClone} from "../../src/FactoryClone/FactoryClone.sol";
 
 interface Vm {
     function expectRevert() external;
+}
+
+contract CloneCounter {
+    uint256 public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+contract FactoryCloneTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    function testDeployCloneAndInteract() external {
