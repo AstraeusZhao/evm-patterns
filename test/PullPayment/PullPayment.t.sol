d1af7efc038518a0d8e4cd50d979306d7827c5ce
Astraeus <astraeuszhao@gmail.com>
2024-10-22 16:53:01 +0800

    test: harden pullpayment pullpayment t sol

--- test/PullPayment/PullPayment.t.sol
+++ test/PullPayment/PullPayment.t.sol
@@ -5,3 +5,11 @@ import {PullPayment} from "../../src/PullPayment/PullPayment.sol";
 
 interface Vm {
     function deal(address account, uint256 newBalance) external;
+    function prank(address sender) external;
+    function expectRevert() external;
+}
+
+contract PullPaymentTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant ALICE = address(0xA11CE);
