441be0b477b9518ae91646af502c684e98ddab30
Astraeus <astraeuszhao@gmail.com>
2024-10-31 19:06:20 +0800

    test: cover more of pullpaymentvault pullpaymentvault t sol

--- test/PullPaymentVault/PullPaymentVault.t.sol
+++ test/PullPaymentVault/PullPaymentVault.t.sol
@@ -9,3 +9,25 @@ interface Vm {
 
 contract PullPaymentVaultTest {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    receive() external payable {}
+
+    function testDepositAndPullWithdraw() external {
+        PullPaymentVault vault = new PullPaymentVault();
+        vm.deal(address(this), 3 ether);
+
+        vault.deposit{value: 3 ether}();
+        require(vault.credits(address(this)) == 3 ether, "credit was not recorded");
+        require(address(vault).balance == 3 ether, "vault balance is wrong");
+
+        vault.withdraw(1 ether);
+        require(vault.credits(address(this)) == 2 ether, "credit was not reduced");
+        require(address(vault).balance == 2 ether, "withdrawal was not paid");
+    }
+
+    function testOwnerCanPauseAndUnpause() external {
+        PullPaymentVault vault = new PullPaymentVault();
+
+        vault.pause();
+        require(vault.paused(), "vault did not pause");
+
