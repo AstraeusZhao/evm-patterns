4a2fc5f6ab47b3034d48400d1391913db4fbef34
Astraeus <astraeuszhao@gmail.com>
2024-11-07 18:54:46 +0800

    test: rework safeerc20wrapper safeerc20wrapper t sol setup

--- test/SafeERC20Wrapper/SafeERC20Wrapper.t.sol
+++ test/SafeERC20Wrapper/SafeERC20Wrapper.t.sol
@@ -9,3 +9,45 @@ interface Vm {
 
 /// A token that returns empty data on transfer (USDT-style quirk).
 contract SilentToken is IERC20Minimal {
+    mapping(address => uint256) public balanceOf;
+    uint256 public totalSupply;
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        assembly {
+            mstore(0, 0)
+            return(0, 0)
+        } // return empty
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        assembly {
+            mstore(0, 0)
+            return(0, 0)
+        }
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        assembly {
+            mstore(0, 0)
+            return(0, 0)
+        }
+    }
+}
+
+contract SafeERC20WrapperTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant OWNER = address(0xB055);
+    address private constant RECIPIENT = address(0xEC1);
+
+    function testSafeTransferHandlesSilentToken() external {
+        SilentToken t = new SilentToken();
