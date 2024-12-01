bbb2a28caee8161f47843e07ca889e37d8612b4d
Astraeus <astraeuszhao@gmail.com>
2024-12-01 18:51:56 +0800

    test: extend storagecollision storagecollision t sol

--- test/StorageCollision/StorageCollision.t.sol
+++ test/StorageCollision/StorageCollision.t.sol
@@ -4,3 +4,14 @@ pragma solidity ^0.8.24;
 import {StorageCollisionProxy, ImplValueSlot0, ImplValueSlot1} from "../../src/StorageCollision/StorageCollision.sol";
 
 interface Vm {
+    function prank(address sender) external;
+}
+
+contract StorageCollisionTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    function _proxy() internal returns (StorageCollisionProxy) {
+        vm.prank(address(0xB055));
+        return new StorageCollisionProxy();
+    }
+
