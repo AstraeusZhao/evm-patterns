55d4da158759a9dc29c66c143ab7d8ca69f0f35f
Astraeus <astraeuszhao@gmail.com>
2025-01-04 15:05:18 +0800

    test: add bondingcurve bondingcurve t sol

---
+++ test/BondingCurve/BondingCurve.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.24;
+
+import {BondingCurve} from "../../src/BondingCurve/BondingCurve.sol";
+
+interface Vm {
+    function deal(address account, uint256 newBalance) external;
+    function prank(address sender) external;
