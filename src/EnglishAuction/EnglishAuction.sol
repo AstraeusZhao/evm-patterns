ec4442f19f0df029a76f06fc51aa99baeb530780
Astraeus <astraeuszhao@gmail.com>
2025-01-21 14:56:16 +0800

    chore: touch up englishauction englishauction sol

--- src/EnglishAuction/EnglishAuction.sol
+++ src/EnglishAuction/EnglishAuction.sol
@@ -6,3 +6,10 @@ pragma solidity ^0.8.24;
 /// @dev CEI ordering: refund the previous bidder before accepting the new bid.
 ///      Not audited.
 contract EnglishAuction {
+    error NotOwner(address caller);
+    error AuctionClosed();
+    error AuctionNotEnded();
+    error BidTooLow(uint256 currentBid, uint256 bid);
+    error TransferFailed();
+
+    event Bid(address indexed bidder, uint256 amount);
