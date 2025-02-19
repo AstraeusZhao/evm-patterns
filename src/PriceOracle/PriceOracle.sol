e17c82d7606cf0b6820e454641431b54f26a3ceb
Astraeus <astraeuszhao@gmail.com>
2025-02-19 09:25:42 +0800

    feat: round out priceoracle priceoracle sol

--- src/PriceOracle/PriceOracle.sol
+++ src/PriceOracle/PriceOracle.sol
@@ -3,3 +3,8 @@ pragma solidity ^0.8.24;
 
 /// @title PriceOracle
 /// @notice Admin-posted price feeds with staleness protection.
+/// @dev Consumers must check the returned timestamp; stale feeds revert.
+///      Not audited.
+contract PriceOracle {
+    error NotAdmin(address caller);
+    error ZeroPrice();
