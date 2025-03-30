55dd32d24d527b0c8f37a7661c82cb56bc95d3b4
Astraeus <astraeuszhao@gmail.com>
2025-03-31 01:52:04 +0800

    test: cover more of bondingcurve bondingcurve t sol

--- test/BondingCurve/BondingCurve.t.sol
+++ test/BondingCurve/BondingCurve.t.sol
@@ -6,3 +6,29 @@ import {BondingCurve} from "../../src/BondingCurve/BondingCurve.sol";
 interface Vm {
     function deal(address account, uint256 newBalance) external;
     function prank(address sender) external;
+    function expectRevert() external;
+}
+
+contract BondingCurveTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant ALICE = address(0xA11CE);
+    address private constant BOB = address(0xB0B);
+
+    receive() external payable {}
+
+    function _curve() internal returns (BondingCurve) {
+        vm.deal(ALICE, 10 ether);
+        vm.deal(BOB, 10 ether);
+        return new BondingCurve();
+    }
+
+    function testBuyMintsTokens() external {
+        BondingCurve c = _curve();
+        vm.prank(ALICE);
+        uint256 minted = c.buy{value: 1 ether}();
+        require(minted > 0, "nothing minted");
+        require(c.supply() == minted, "supply mismatch");
+        require(c.reserve() == 1 ether, "reserve mismatch");
+    }
+
