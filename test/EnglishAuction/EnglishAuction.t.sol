a8d869c7e8d6773c9f2e46e4f38a27635cee9680
Astraeus <astraeuszhao@gmail.com>
2025-04-15 17:22:58 +0800

    test: add cases for englishauction englishauction t sol

--- test/EnglishAuction/EnglishAuction.t.sol
+++ test/EnglishAuction/EnglishAuction.t.sol
@@ -14,3 +14,18 @@ contract EnglishAuctionTest {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
     address private constant SELLER = address(0xB055);
+    address private constant ALICE = address(0xA11CE);
+    address private constant BOB = address(0xB0B);
+
+    function _auction() internal returns (EnglishAuction) {
+        vm.deal(ALICE, 10 ether);
+        vm.deal(BOB, 10 ether);
+        vm.prank(SELLER);
+        return new EnglishAuction(1 days);
+    }
+
+    function testBidAndOutbid() external {
+        EnglishAuction a = _auction();
+        vm.prank(ALICE);
+        a.bid{value: 1 ether}();
+        require(a.highestBidder() == ALICE, "first bidder not recorded");
