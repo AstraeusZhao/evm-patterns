47f9bb4440bdb11821b542a0848f02a08a8d99a7
Astraeus <astraeuszhao@gmail.com>
2025-04-20 00:36:18 +0800

    test: tighten merkleairdrop merkleairdrop t sol assertions

--- test/MerkleAirdrop/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop/MerkleAirdrop.t.sol
@@ -10,3 +10,14 @@ interface Vm {
 
 contract MockToken is IERC20Like {
     mapping(address => uint256) public balanceOf;
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+}
