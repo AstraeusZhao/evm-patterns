e63475a31d6de7255f705153a29971a326f5b5f2
Astraeus <astraeuszhao@gmail.com>
2025-04-20 18:43:57 +0800

    test: stabilize merkleairdrop merkleairdrop t sol

--- test/MerkleAirdrop/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop/MerkleAirdrop.t.sol
@@ -21,3 +21,26 @@ contract MockToken is IERC20Like {
         return true;
     }
 }
+
+contract MerkleAirdropTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant ALICE = address(0xA11CE);
+    address private constant BOB = address(0xB0B);
+    address private constant OWNER = address(0xB055);
+
+    function _airdrop() internal returns (MerkleAirdrop, MockToken, bytes32[] memory, bytes32[] memory) {
+        // Tree with 4 leaves: (ALICE,100), (BOB,50), (CAROL,25), (DAVE,10)
+        bytes32 leafA = keccak256(bytes.concat(keccak256(abi.encode(ALICE, uint256(100)))));
+        bytes32 leafB = keccak256(bytes.concat(keccak256(abi.encode(BOB, uint256(50)))));
+        bytes32 leafC = keccak256(bytes.concat(keccak256(abi.encode(address(0xC001), uint256(25)))));
+        bytes32 leafD = keccak256(bytes.concat(keccak256(abi.encode(address(0xD4E), uint256(10)))));
+
+        bytes32 ab =
+            leafA < leafB ? keccak256(abi.encodePacked(leafA, leafB)) : keccak256(abi.encodePacked(leafB, leafA));
+        bytes32 cd =
+            leafC < leafD ? keccak256(abi.encodePacked(leafC, leafD)) : keccak256(abi.encodePacked(leafD, leafC));
+        bytes32 root = ab < cd ? keccak256(abi.encodePacked(ab, cd)) : keccak256(abi.encodePacked(cd, ab));
+
+        MockToken t = new MockToken();
+        t.mint(address(this), 1000 ether);
