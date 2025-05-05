5bdc783727f8573906bc69628b3a61e4a5902188
Astraeus <astraeuszhao@gmail.com>
2025-05-05 19:09:18 +0800

    test: extend priceoracle priceoracle t sol

--- test/PriceOracle/PriceOracle.t.sol
+++ test/PriceOracle/PriceOracle.t.sol
@@ -5,3 +5,10 @@ import {PriceOracle} from "../../src/PriceOracle/PriceOracle.sol";
 
 interface Vm {
     function prank(address sender) external;
+    function warp(uint256 newTimestamp) external;
+    function expectRevert() external;
+}
+
+contract PriceOracleTest {
+    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
