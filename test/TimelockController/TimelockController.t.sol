07efd17f6a12d452b6174432a0eae590ca407c12
Astraeus <astraeuszhao@gmail.com>
2025-07-12 21:29:12 +0800

    test: tighten timelockcontroller timelockcontroller t sol assertions

--- test/TimelockController/TimelockController.t.sol
+++ test/TimelockController/TimelockController.t.sol
@@ -22,3 +22,27 @@ contract Target {
 
 contract TimelockControllerTest {
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    address private constant ADMIN = address(0xA0A);
+    address private constant PROPOSER = address(0xA5E);
+    address private constant EXECUTOR = address(0xECC);
+
+    uint256 private constant DELAY = 2 days;
+    uint256 private start;
+
+    receive() external payable {}
+
+    function _timelock() internal returns (TimelockController) {
+        address[] memory proposers = new address[](1);
+        proposers[0] = PROPOSER;
+        address[] memory executors = new address[](1);
+        executors[0] = EXECUTOR;
+
+        start = block.timestamp;
+        vm.warp(start);
+        return new TimelockController(DELAY, proposers, executors, ADMIN);
+    }
+
+    function _setValueData() internal pure returns (bytes memory) {
+        return abi.encodeCall(Target.setValue, (42));
+    }
